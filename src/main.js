863f1f94d14ffd765bb0a4c86e74d09132e6f936
l3onardodev <leonardo.contato03@gmail.com>
2021-11-06 23:36:04 -0300

    feat: Header page almost done in mobile devices

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../scss/custom.scss';
+
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="container-xl" style="border: 1px solid red">
+      <div class="row py-3 align-items-center justify-content-between" style="border: 1px solid red">
+        <div class="col-5" style="border: 1px solid green">
+          <img src="../assets/images/logo.svg" class="img-fluid" v-once>
+        </div>
+        <div class="col-5" style="border: 1px solid green">
+          <b-button variant="outline-secondary">Try it free</b-button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col p-4">
+          <div class="row mt-5">
+            <h4 class="text-center w-100">Build the Community your fans Will Love</h4>
+          </div>
+          <div class="row">
+            <p class="text-center text-info my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus magni labore a dolore ullam deserunt corporis, saepe cumque expedita officiis laudantium provident ea ad perspiciatis aliquid commodi. Sit, consequuntur optio.</p>
+          </div>
+
+          <div class="row justify-content-center my-4">
+            <b-button variant="success">Get Started For Free</b-button>
+          </div>
+
+          <div class="row my-5">
+            <img src="../assets/images/screen-mockups.svg" class="img-fluid" v-once>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
