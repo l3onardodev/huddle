1549abc611956d7b9466ec58c20b7c8c3a77336a
l3onardodev <leonardo.contato03@gmail.com>
2021-11-07 19:40:40 -0300

    feat: All sections created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container-fluid">
-    <div class="container-xl" style="border: 1px solid red">
-      <div class="row py-3 align-items-center justify-content-between" style="border: 1px solid red">
-        <div class="col-5" style="border: 1px solid green">
+  <div class="container-fluid p-0">
+    <section class="container-xl">
+      <div class="row py-3 align-items-center justify-content-between">
+        <div class="col-5">
           <img src="../assets/images/logo.svg" class="img-fluid" v-once>
         </div>
-        <div class="col-5" style="border: 1px solid green">
+        <div class="col-5">
           <b-button variant="outline-secondary">Try it free</b-button>
         </div>
       </div>
@@ -19,7 +19,7 @@
           </div>
 
           <div class="row justify-content-center my-4">
-            <b-button variant="success">Get Started For Free</b-button>
+            <b-button variant="secondary" class="font-weight-bold">Get Started For Free</b-button>
           </div>
 
           <div class="row my-5">
@@ -27,7 +27,94 @@
           </div>
         </div>
       </div>
-    </div>
+      <div class="row">
+        <div class="col justify-content-end">
+
+          <div class="row justify-content-center mt-4">
+            <div class="row d-flex flex-column align-items-center">
+              <div>
+                <img src="../assets/images/icon-communities.svg" class="w-25" v-once>
+                <p class="display-4 text-info font-weight-bold">1.4k+</p>
+              </div>
+              <p class="mt-2">Communities Formed</p>
+            </div>
+          </div>
+
+          <div class="row justify-content-center my-5">
+            <div class="row d-flex flex-column align-items-center">
+              <div>
+                <img src="../assets/images/icon-messages.svg" class="w-25" v-once>
+                <p class="display-4 text-info font-weight-bold">2.7m+</p>
+              </div>
+              <p class="mt-2">Messages Sent</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="hsl(207, 100%, 98%)" fill-opacity="1" d="M0,64L48,101.3C96,139,192,213,288,245.3C384,277,480,267,576,245.3C672,224,768,192,864,170.7C960,149,1056,139,1152,133.3C1248,128,1344,128,1392,128L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
+    <section class="container-xl p-0 bg-light">
+      <div class="col">
+        <div class="row justify-content-center pt-5">
+          <img src="../assets/images/illustration-grow-together.svg" class="img-fluid w-75 mb-5" v-once>
+
+          <h4 class="text-info my-4">Grow together</h4>
+          <p class="text-center text-info px-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error neque commodi recusandae qui autem nesciunt tenetur labore</p>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 320"><path fill="#fff" fill-opacity="1" d="M0,128L48,154.7C96,181,192,235,288,224C384,213,480,139,576,106.7C672,75,768,85,864,74.7C960,64,1056,32,1152,58.7C1248,85,1344,171,1392,213.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+
+    <section class="container-xl p-0">
+      <div class="col">
+        <div class="row pt-5 justify-content-center">
+          <img src="../assets/images/illustration-flowing-conversation.svg" class="img-fluid w-75 mb-5">
+
+          <h5 class="text-info pt-5">Flowing Conversations</h5>
+          <p class="text-center text-info px-4 py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum blanditiis optio molestiae totam, atque.</p>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="50 0 740 320">
+        <path fill="hsl(207, 100%, 98%)" fill-opacity="1" d="M0,256L48,213.3C96,171,192,85,288,96C384,107,480,213,576,250.7C672,288,768,256,864,229.3C960,203,1056,181,1152,192C1248,203,1344,245,1392,266.7L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+    </section>
+
+    <section class="container-xl p-0 bg-light">
+      <div class="col">
+        <div class="row justify-content-center">
+          <img src="../assets/images/illustration-your-users.svg" class="img-fluid w-75 my-4" v-once>
+
+          <h4 class="text-info mt-5">Your users</h4>
+          <p class="text-center text-info px-4 py-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus sunt, ipsam maiores corporis laudantium explicabo, est fuga natus mollitia quas ducimus.</p>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="100 0 740 320">
+        <path fill="#fff" fill-opacity="1" d="M0,256L48,213.3C96,171,192,85,288,96C384,107,480,213,576,250.7C672,288,768,256,864,229.3C960,203,1056,181,1152,192C1248,203,1344,245,1392,266.7L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+    </section>
+
+    <section class="container-xl p-0">
+      <div class="col my-5">
+        <div class="row justify-content-center">
+          <h3 class="text-center my-5">Ready To Build Your Community?</h3>
+          <b-button variant="secondary" class="font-weight-bold mb-5">Get Started For Free</b-button>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1040 220" class="mt-5"><path fill="hsl(192, 100%, 9%)" fill-opacity="1" d="M0,64L40,90.7C80,117,160,171,240,176C320,181,400,139,480,101.3C560,64,640,32,720,16C800,0,880,0,960,10.7C1040,21,1120,43,1200,90.7C1280,139,1360,213,1400,250.7L1440,288L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+    </section>
+
+    <section class="container-xl p-4 bg-info text-white">
+      <div class="col">
+        <div class="row">
+          <h4 class="text-white font-weight-light">NEWSLETTER</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptates repellat sint suscipit.</p>
+
+          <b-form-input class="rounded"></b-form-input>
+          <b-button variant="secondary" class="rounded align-self-end align-self-end">Subscribe</b-button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
